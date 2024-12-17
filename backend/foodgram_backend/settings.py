f81c12af6ffe7de83ffac908ff5b0b74e977360f
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2024-12-17 13:16:37 +0300

    some github actions changes

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -101,9 +101,10 @@ USE_I18N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = '/media'
 
 STATIC_URL = "static/"
+STATIC_ROOT = '/backend_static/static'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
