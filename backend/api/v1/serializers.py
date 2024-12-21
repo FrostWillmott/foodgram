4c338980ae95435f7cf45ca4229694658e5e30ba
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2024-12-22 00:12:30 +0300

    Preproduction

--- backend/api/v1/serializers.py
+++ backend/api/v1/serializers.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 
 from rest_framework.exceptions import ValidationError
