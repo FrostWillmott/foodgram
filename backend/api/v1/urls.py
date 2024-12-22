d24d4a02415232727dabe47b1218162b9237a9ee
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2024-12-22 13:39:07 +0300

    Preproduction

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -5,13 +5,13 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.routers import DefaultRouter
 
 from foodgram_backend.settings import MEDIA_ROOT, MEDIA_URL
+from recipes.models import Recipe
 from .views import (
-    UserViewSet,
     IngredientViewSet,
     RecipeViewSet,
     TagViewSet,
+    UserViewSet,
 )
-from recipes.models import Recipe
 
 
 def shortlink_redirect_view(request, short_link):
@@ -26,8 +26,10 @@ router.register("recipes", RecipeViewSet, basename="recipe")
 router.register("ingredients", IngredientViewSet, basename="ingredient")
 
 urlpatterns = [
+    path(
+        "<str:short_link>/", shortlink_redirect_view, name="recipe-shortlink"
+    ),
     path("", include(router.urls)),
     path("auth/", include("djoser.urls.authtoken")),
     path("auth/login/", include("django.contrib.auth.urls")),
-    path("<str:short_link>/", shortlink_redirect_view, name="recipe-shortlink"),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
