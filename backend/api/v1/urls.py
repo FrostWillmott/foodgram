b3362996bd5a508637b7dabfb4ccf064b543141a
FrostWillmott <121764288+FrostWillmott@users.noreply.github.com>
2024-12-22 12:31:25 +0300

    Preproduction

--- backend/api/v1/urls.py
+++ backend/api/v1/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
 from foodgram_backend.settings import MEDIA_ROOT, MEDIA_URL
@@ -20,4 +20,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("auth/", include("djoser.urls.authtoken")),
     path("auth/login/", include("django.contrib.auth.urls")),
+    re_path(r"^r/(?P<short_link>[^/]+)/$", RecipeViewSet.as_view({'get': 'redirect_to_recipe'}), name="redirect_to_recipe"),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
